f5b73fa690d2f1a8522db3f44ade82e079b2cce2
Brian Loveless <Brian.loveless1@yahoo.com>
2021-08-25 10:54:38 -0400

    works on internal data

--- src/com/BrianLoveless1/Main.java
+++ src/com/BrianLoveless1/Main.java
@@ -1,9 +1,46 @@
 package com.BrianLoveless1;
 
+import javax.swing.*;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-        System.out.println("howdy");
+        // write your code here
+        // System.out.println("howdy");
+
+        String ID = "0003";
+        String firstName = "Ben";
+        String lastName = "Franklin";
+        String state = "Massachusetts Bay Colony";
+        String email = "bornbeforewehadstates@usa.gov";
+
+        String filepath = "data.txt";
+
+        saveRecord(ID, firstName, lastName, state, email, filepath);
+
+    }
+
+    public static void saveRecord(String ID, String firstName, String lastName, String state, String email, String filepath) {
+        try {
+            FileWriter fw = new FileWriter(filepath, true);
+            BufferedWriter bw = new BufferedWriter(fw);
+            PrintWriter pw = new PrintWriter(bw);
+
+            pw.println(ID + "," + firstName + "," + lastName + "," + state + "," + email);
+
+            pw.flush();
+            pw.close();
+
+            JOptionPane.showMessageDialog(null, "record success file updated");
+
+
+        } catch (Exception E) {
+            JOptionPane.showMessageDialog(null, "error record not saved");
+        }
+
+
     }
 }
